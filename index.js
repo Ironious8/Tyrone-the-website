953b7393e18e2835505bdc5f69b2c2d54f961fe9
Ironious8 <tyronekariuki@gmail.com>
2024-06-25 14:03:59 +0300

    Add profile

---
+++ index.js
@@ -0,0 +1,10 @@
+const firstName="Tyrone";
+const secondName="Kariuki";
+const thirdName="Wahu";
+let fullName=firstName + " " + secondName + " " + thirdName;
+console.log(fullName);
+
+ const answer='false';
+ console.log(answer);
+ 
+ 
